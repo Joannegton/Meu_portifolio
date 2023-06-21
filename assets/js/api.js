ca0f460ff4afa7848889d7afd4a16f0a6c0a50a3
Joannegton <72559274+Joannegton@users.noreply.github.com>
2023-06-21 02:52:53 -0300

    json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/Joannegton/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Joannegton/meu_portifolio/main/data/dados.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
